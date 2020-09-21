999fe9c6d8151fca16bd175041655c7ff66898b9
Durgesh Nandan Sinha <dnsinha33@gmail.com>
2020-09-21 14:15:39 +0530

    dynamic value for triplet

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import Box from 'Components/common/box';
 import './style.scss';
 
+const getParameter = (name, search) => {
+  const match = RegExp('[?&]' + name + '=([^&]*)').exec(search);
+  return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+}
+
 export class Home extends React.Component {
 
   constructor(props) {
@@ -15,7 +20,17 @@ export class Home extends React.Component {
   }
 
   componentDidMount() {
-    
+    let search = this.props.history.location && this.props.history.location.search;
+    let a = getParameter('a', search);
+    let b = getParameter('b', search);
+    let c = getParameter('c', search);
+    let isTriplet = a ** 2 + b ** 2 === c ** 2;
+    this.setState({
+      perpendicular: a,
+      base: b,
+      hypotenuse: c,
+      isTriplet
+    });
   }
 
 
@@ -23,15 +38,20 @@ export class Home extends React.Component {
     this.hypotenuseStyle = this.state.perpendicular < this.state.base ? 'rotate-more' : 'rotate-less';
     return (
       <div className="container large-container">
-        <div className={"hypotenuse " + this.hypotenuseStyle}>
-          <Box value={this.state.hypotenuse} base={this.state.base} />
-        </div>
-        <div className="perpendicular">
-          <Box value={this.state.perpendicular} css="blue" />
-        </div>
-        <div className="base">
-          <Box value={this.state.base} css="green" />
-        </div>
+        {this.state.isTriplet ? <div>
+          <div className={"hypotenuse " + this.hypotenuseStyle}>
+            <Box value={this.state.hypotenuse} base={this.state.base} />
+          </div>
+          <div className="perpendicular">
+            <Box value={this.state.perpendicular} css="blue" />
+          </div>
+          <div className="base">
+            <Box value={this.state.base} css="green" />
+          </div>
+        </div> : <div>
+            <p>Please enter a valid triplet For Eg: a: 3, b: 4, c: 5</p>
+          </div>}
+
       </div>
     );
   }
